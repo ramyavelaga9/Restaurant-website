befe498927b5c082dbbf2e8d519dfa1191772752
ramyavelaga9 <cs16b032@iittp.ac.in>
2020-07-02 19:45:43 +0530

    configured angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
